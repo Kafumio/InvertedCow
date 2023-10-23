ea722129c28489aa5755ab9055ba529530679537
fzw-code <81961566+fzw-code@users.noreply.github.com>
2023-10-24 01:19:15 +0800

    fix: 修复一下email相关bug

--- utils/email_util.go
+++ utils/email_util.go
@@ -13,10 +13,10 @@ type EmailMessage struct {
 
 func SendMail(config *config.EmailConfig, message EmailMessage) error {
 	m := gomail.NewMessage()
-	m.SetHeader("From", m.FormatAddress(config.User, "fancode")) // 添加别名
-	m.SetHeader("To", message.To...)                             // 发送给用户(可以多个)
-	m.SetHeader("Subject", message.Subject)                      // 设置邮件主题
-	m.SetBody("text/html", message.Body)                         // 设置邮件正文
+	m.SetHeader("From", m.FormatAddress(config.User, "InvertedCow")) // 添加别名
+	m.SetHeader("To", message.To...)                                 // 发送给用户(可以多个)
+	m.SetHeader("Subject", message.Subject)                          // 设置邮件主题
+	m.SetBody("text/html", message.Body)                             // 设置邮件正文
 	d := gomail.NewDialer(config.Host, config.Port, config.User, config.Password)
 	err := d.DialAndSend(m)
 	return err
